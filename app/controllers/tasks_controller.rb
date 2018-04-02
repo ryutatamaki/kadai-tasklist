7d02b1df76e7bc522ee90e3baa4c96d1cf636775
EC2 Default User <ec2-user@ip-172-31-43-15.us-east-2.compute.internal>
2018-04-02 10:43:20 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
@@ -12,14 +15,14 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "タスクを追加しました"
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = "タスクを追加できませんでした"
-      render :new
+      render 'toppages/index'
     end
   end
 
@@ -39,16 +42,21 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    
     flash[:success] = "タスクを削除しました"
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,9 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
-
-  resources :tasks
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :tasks, only: [:create, :destroy]
 end
