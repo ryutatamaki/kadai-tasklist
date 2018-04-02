1f9a1b05ffbb312827411f08533a96b405de8c89
EC2 Default User <ec2-user@ip-172-31-43-15.us-east-2.compute.internal>
2018-04-02 09:45:39 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root 'toppages#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 
   resources :tasks
   
