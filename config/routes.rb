81413a1ee462b3ccb28ce5ce0fb1b98065689fb5
EC2 Default User <ec2-user@ip-172-31-43-15.us-east-2.compute.internal>
2018-04-02 08:32:28 +0000

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root "tasks#index"
+
+  root 'toppages#index'
+
   resources :tasks
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
